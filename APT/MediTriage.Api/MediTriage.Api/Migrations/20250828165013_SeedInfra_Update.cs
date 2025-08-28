3cff7b0bc21a663fdf2d29b956eb09957840f545
mcuevas8 <matias.cuevas@provida.cl>
2025-08-28 15:45:36 -0400

    feat(seed): mejorar seeder con Bogus para poblar doctores, pacientes y citas demo

---
+++ APT/MediTriage.Api/MediTriage.Api/Migrations/20250828165013_SeedInfra_Update.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediTriage.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInfra_Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
