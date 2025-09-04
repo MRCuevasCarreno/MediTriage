c320f1901e54060919ea02c7a3717befe7668316
mcuevas8 <matias.cuevas@provida.cl>
2025-09-04 15:20:20 -0400

    Modelo vista base de datos Relacional.

---
+++ APT/MediTriage.Api/MediTriage.Api/AppDbContext.sql
@@ -0,0 +1,165 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Users] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Email] nvarchar(450) NOT NULL,
+        [Role] int NOT NULL,
+        CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Doctors] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] int NOT NULL,
+        [Specialty] nvarchar(max) NOT NULL,
+        [Center] nvarchar(max) NULL,
+        CONSTRAINT [PK_Doctors] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Doctors_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Patients] (
+        [Id] int NOT NULL IDENTITY,
+        [UserId] int NOT NULL,
+        [DateOfBirth] datetime2 NULL,
+        CONSTRAINT [PK_Patients] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Patients_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Appointments] (
+        [Id] int NOT NULL IDENTITY,
+        [PatientId] int NOT NULL,
+        [DoctorId] int NOT NULL,
+        [Start] datetime2 NOT NULL,
+        [End] datetime2 NOT NULL,
+        [Status] int NOT NULL,
+        [TriageLevel] nvarchar(max) NULL,
+        [TriageNotes] nvarchar(max) NULL,
+        CONSTRAINT [PK_Appointments] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Appointments_Doctors_DoctorId] FOREIGN KEY ([DoctorId]) REFERENCES [Doctors] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_Appointments_Patients_PatientId] FOREIGN KEY ([PatientId]) REFERENCES [Patients] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Appointments_DoctorId] ON [Appointments] ([DoctorId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Appointments_PatientId] ON [Appointments] ([PatientId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Doctors_UserId] ON [Doctors] ([UserId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Patients_UserId] ON [Patients] ([UserId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Users_Email] ON [Users] ([Email]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250825203543_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250825203543_InitialCreate', N'9.0.8');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250828165013_SeedInfra_Update'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250828165013_SeedInfra_Update', N'9.0.8');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250828213247_Auth_AddPasswordHash'
+)
+BEGIN
+    DECLARE @var sysname;
+    SELECT @var = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Users]') AND [c].[name] = N'Name');
+    IF @var IS NOT NULL EXEC(N'ALTER TABLE [Users] DROP CONSTRAINT [' + @var + '];');
+    ALTER TABLE [Users] ALTER COLUMN [Name] nvarchar(120) NOT NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250828213247_Auth_AddPasswordHash'
+)
+BEGIN
+    ALTER TABLE [Users] ADD [PasswordHash] nvarchar(max) NOT NULL DEFAULT N'';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250828213247_Auth_AddPasswordHash'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250828213247_Auth_AddPasswordHash', N'9.0.8');
+END;
+
+COMMIT;
+GO
