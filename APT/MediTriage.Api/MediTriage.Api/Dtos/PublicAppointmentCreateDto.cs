5c5ba3010ddb629243ee428e3fc705327721fbd9
mcuevas8 <matias.cuevas@provida.cl>
2025-09-30 10:16:32 -0300

    Agregar API que permite crear citas sin estar registrado

---
+++ APT/MediTriage.Api/MediTriage.Api/Dtos/PublicAppointmentCreateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MediTriage.Api.Dtos
+{
+    public class PublicAppointmentCreateDto
+    {
+        public string FullName { get; set; } = string.Empty;   // nombre paciente
+        public string Email { get; set; } = string.Empty;      // email paciente
+        public int DoctorId { get; set; }                      // doctor destino
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        // Opcionales, por si quieres triage inicial
+        public string? TriageLevel { get; set; }
+        public string? TriageNotes { get; set; }
+    }
+}
